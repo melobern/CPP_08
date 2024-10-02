2cc16b208004a5d89ece039adf1b1f001598d154
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2024-10-02 10:52:09 +0200

    ex02 cpplint

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -22,8 +22,10 @@
 #define RESET "\033[0m"
 
 static void subjectTests(void) {
-  std::cout << CYAN "------------Subject tests------------" RESET << std::endl;
+  std::cout << YELLOW "------------Subject tests------------" RESET;
+  std::cout << std::endl;
   MutantStack<int> mstack;
+
   mstack.push(5);
   mstack.push(17);
   std::cout << mstack.top() << std::endl;
@@ -42,10 +44,132 @@ static void subjectTests(void) {
     ++it;
   }
   std::stack<int> s(mstack);
+  std::cout << std::endl;
+}
+
+void emptyTest(void) {
+  std::cout << YELLOW << "--------Empty MutantStack test-------" << RESET;
+  std::cout << std::endl;
+  {
+    std::cout << CYAN << "begin & end" << RESET << std::endl;
+    MutantStack<int> mstack;
+    MutantStack<int>::iterator itBegin = mstack.begin();
+    MutantStack<int>::iterator itEnd = mstack.end();
+    bool result = itBegin == itEnd;
+    std::cout << "Resultat: " << result << std::endl;
+  }
+  {
+    std::cout << CYAN << "rbegin & rend" << RESET << std::endl;
+    MutantStack<int> mstack;
+    MutantStack<int>::reverse_iterator itBegin = mstack.rbegin();
+    MutantStack<int>::reverse_iterator itEnd = mstack.rend();
+    bool result = itBegin == itEnd;
+    std::cout << "Resultat: " << result << std::endl;
+    std::cout << std::endl;
+  }
+}
+
+void copyTest(void) {
+  std::cout << YELLOW << "Copy constructor & copy assignment operator "
+            << RESET << std::endl;
+  MutantStack<double> mstack;
+  mstack.push(1.2);
+  mstack.push(42.0);
+  std::cout << "Original: " << std::endl;
+  MutantStack<double>::const_iterator it = mstack.begin();
+  MutantStack<double>::const_iterator ite = mstack.end();
+  while (it != ite) {
+    std::cout << *it << std::endl;
+    ++it;
+  }
+  std::cout << std::endl << "Copy constructor: " << std::endl;
+  MutantStack<double> copy(mstack);
+  MutantStack<double>::const_iterator it2 = copy.begin();
+  MutantStack<double>::const_iterator ite2 = copy.end();
+  while (it2 != ite2) {
+    std::cout << *it2 << std::endl;
+    ++it2;
+  }
+  std::cout << std::endl << "Copy assignment operator: " << std::endl;
+  MutantStack<double> copy2;
+  copy2 = mstack;
+  MutantStack<double>::const_iterator it3 = copy2.begin();
+  MutantStack<double>::const_iterator ite3 = copy2.end();
+  while (it3 != ite3) {
+    std::cout << *it3 << std::endl;
+    ++it3;
+  }
+  std::cout << std::endl;
+}
+
+void otherTypesTest(void) {
+  std::cout << std::endl
+            << YELLOW << "Test with other types " << RESET << std::endl;
+  {
+    std::cout << CYAN << "Elements type: char" << RESET << std::endl;
+    MutantStack<char> mstack;
+    mstack.push('A');
+    mstack.push('B');
+    std::cout << "< Before pop > top: " << mstack.top();
+    std::cout << " | size: " << mstack.size() << std::endl;
+    mstack.pop();
+    std::cout << "< After pop > top: " << mstack.top();
+    std::cout << " | Size: " << mstack.size() << std::endl;
+    mstack.push('C');
+    mstack.push('D');
+    mstack.push('E');
+    mstack.push('!');
+    MutantStack<char>::iterator it = mstack.begin();
+    MutantStack<char>::iterator ite = mstack.end();
+    while (it != ite) {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+  }
+  {
+    std::cout << CYAN << "Elements type: std::string" << RESET << std::endl;
+    MutantStack<std::string> mstack;
+    mstack.push("Hello");
+    mstack.push("Hey");
+    std::cout << "< Before pop > top: " << mstack.top();
+    std::cout << " | size: " << mstack.size() << std::endl;
+    mstack.pop();
+    std::cout << "< After pop > top: " << mstack.top();
+    std::cout << " | Size: " << mstack.size() << std::endl;
+    mstack.push("Salut");
+    mstack.push("Chao");
+    mstack.push("Hola");
+    mstack.push("Guten Tag");
+    MutantStack<std::string>::iterator it = mstack.begin();
+    MutantStack<std::string>::iterator ite = mstack.end();
+    while (it != ite) {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+  }
+}
+
+void iteratorValidityTest(void) {
+  std::cout << std::endl << YELLOW << "Iterator test " << RESET << std::endl;
+  MutantStack<float> mstack;
+  mstack.push(1.1f);
+  mstack.push(2.2f);
+  mstack.push(3.3f);
+  MutantStack<float>::iterator it = mstack.end();
+  --it;
+  std::cout << *it << std::endl;
+  mstack.push(4.4f);
+  std::cout << *it << std::endl;
+  mstack.pop();
+  std::cout << *it << std::endl;
 }
 
 int main(void) {
   subjectTests();
+  emptyTest();
+  copyTest();
+  otherTypesTest();
+  iteratorValidityTest();
 
   return (0);
 }
